48f9d302d4f0a23c3a6e4b870ea40c24c9b1febc
Austin Quach <austinquachh@gmail.com>
2018-04-20 00:06:56 -0700

    finished

--- main.kts
+++ main.kts
@@ -5,8 +5,15 @@ println("UW Complex Kotlin homework")
 // use fold to compress the array of strings down into a single string
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
 //
-val mapFoldResults = ""
-
+val mapFoldResults = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+        .map {
+            it: Int -> when {
+                (it % 3 == 0) && (it % 5 == 0) -> "FIZZBUZZ"
+                it % 3 == 0 -> "FIZZ"
+                it % 5 == 0 -> "BUZZ"
+            else -> ""
+            }
+        }.fold("", {acc, next -> acc + next})
 
 // This is a utility function for your use as you choose, and as an
 // example of an extension method
@@ -20,21 +27,37 @@ fun Int.times(block: () -> Unit): Unit {
 fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
-val r1 = "" // call process() with message "FOO" and a block that returns "BAR"
+val r1 = process("FOO", {"BAR"}) // call process() with message "FOO" and a block that returns "BAR"
 
 val r2_message = "wooga"
-val r2 = "" // call process() with message "FOO" and a block that upper-cases 
+val r2 = process("FOO", {r2_message.toUpperCase().repeat(3)}) // call process() with message "FOO" and a block that upper-cases
             // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher {
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString() = "Deep thoughts...."
+    },
+
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString() = "Allow me to suggest an idea..."
+    };
+
+    abstract fun signal(): Philosopher
+    override abstract fun toString(): String
+}
 
 // create an class "Command" that can be used as a function (provide an "invoke()" function)
 // that takes a single parameter ("message" of type String)
 // primary constructor should take a String argument ("prompt")
 // when called, the Command object should return a String containing the prompt and then the message
 class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return prompt + message
+    }
 }
 
 
